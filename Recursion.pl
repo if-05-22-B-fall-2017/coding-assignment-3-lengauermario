984a4d71cef6f9f26caca4f0b5420ee13f83beac
lengauermario <32167396+lengauermario@users.noreply.github.com>
2017-12-07 14:23:07 +0100

    Update Recursion.pl

--- Recursion.pl
+++ Recursion.pl
@@ -6,14 +6,14 @@ tail([_|Tail], Tail).
 prepend([], E, [E]).
 prepend([Head|Tail], E, [E |[Head|Tail]]).
 
-/*addElement add end  funktioniert nicht*/
+/*addElement add end*/
 addElement([], E, [E]).
 addElement([Head|Tail], E, [Head|NewTail]):- addElement(Tail, E, NewTail).
 
-/*hasLength   funktioniert nicht*/
+/*hasLength*/
 hasLength([], 0).
 hasLength([_|Tail], Count):- hasLength(Tail, Y), Count is Y +1.
 
-/*remove remove element from list*/
+/*remove element from list*/
 remove([Head|Tail], Head, Tail).
 remove([Head|Tail], E, [Head|NewTail]):- remove(Tail, E, NewTail).
